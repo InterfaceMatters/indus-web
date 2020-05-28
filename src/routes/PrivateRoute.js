a6b6f0a30fe42b26223fb8a2f05cb282c2b41a5c
pratik <pratik@interfacem.com>
2020-05-28 10:07:27 +0530

    - Added orgId, userId check on routes.

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -4,11 +4,14 @@ import { Redirect, Route } from 'react-router-dom';
 const PrivateRoute = props => {
   const { component: Component, user, ...rest } = props;
 
+  const userId = localStorage.getItem('indusUser');
+  const orgId = localStorage.getItem('indusOrg');
+
   return (
     <Route
       {...rest}
       render={({ location }) =>
-        user !== null ? (
+        user !== null && userId && orgId ? (
           <Component />
         ) : (
           <Redirect
